b8e94695b95e5a31d324bd571bc68958b1a9cb3b
Dougdree <douglas.carreno@unl.edu.ec>
2024-11-29 10:24:47 -0500

    Enumerador

---
+++ Gestion de Eventos/src/Estado.java
@@ -0,0 +1,6 @@
+// Enumerador Estado: define los posibles estados de una invitación
+public enum Estado {
+    ACEPTADA,    // La invitación fue aceptada
+    RECHAZADA,   // La invitación fue rechazada
+    PENDIENTE    // La invitación está pendiente de respuesta
+}
